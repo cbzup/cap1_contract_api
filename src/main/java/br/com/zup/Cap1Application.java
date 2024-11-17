dcbecddec310675c0d1c4ceca2a39e6ff1ed5f39
cbarbosagomesfilho <cbarbosagomesfilho@gmail.com>
2024-11-17 17:14:32 -0300

    ïnitial commit project structure

---
+++ src/main/java/br/com/zup/Cap1Application.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap1Application.class, args);
+	}
+
+}
